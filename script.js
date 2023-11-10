bc600b3581551ce48e0fc1e22f27c3a3f9803faa
macbook <macbook@macbooks-MacBook-Pro.local>
2023-11-10 17:04:26 +0100

    New Changes

--- script.js
+++ script.js
@@ -14,6 +14,107 @@ updateScoreElement();
 //     };
 // }
 
+function resetScore() {
+  score.Wins = 0;
+  score.Losses = 0;
+  score.Draws = 0;
+  localStorage.removeItem("score");
+  updateScoreElement();
+}
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+
+    //when the game is playing, change text to stop playing
+    document.querySelector(".js-auto-play-btn").innerHTML = "Stop Playing";
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+
+    //when the game is not playing, change text to AutoPlay
+    document.querySelector(".js-auto-play-btn").innerHTML = "Auto Playing";
+  }
+}
+
+      document.querySelector(".js-auto-play-btn").addEventListener("click", () => {
+        autoPlay();
+      });
+
+      document.querySelector(".js-rock-btn").addEventListener("click", () => {
+        playGame("rock");
+      });
+
+      document.querySelector(".js-paper-btn").addEventListener("click", () => {
+        playGame("paper");
+      });
+
+      document.querySelector(".js-scissors-btn").addEventListener("click", () => {
+        playGame("scissors");
+      });
+
+// using keyboard to play the game
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "r") {
+    playGame("rock");
+  } else if (event.key === "p") {
+    playGame("paper");
+  } else if (event.key === "s") {
+    playGame("scissors");
+  } else if (event.key === "a") {
+    autoPlay();
+  } else if (event.key == "Backspace") {
+    showResetConfirmation();
+  }
+});
+
+//update the click event listeners to show the confirm. messg.
+//instead of reseting the score immediately.
+
+document.querySelector(".btn-reset-btn").addEventListener("click", () => {
+  showResetConfirmation();
+});
+
+//function for showing confirmation message
+function showResetConfirmation() {
+  document.querySelector(".js-reset-confirmation").innerHTML = `
+  Are you sure you want to reset the score?
+  <button class="js-reset-confirm-yes reset-confirm-btn">
+    Yes
+  </button>
+  
+  <button class="js-reset-confirm-no reset-confirm-btn">
+    No
+  </button>
+  `;
+
+  // add onclick on the reset button
+  document
+    .querySelector(".js-reset-confirm-yes")
+    .addEventListener("click", () => {
+      resetScore();
+      hideResetConfirmation();
+    });
+
+  document
+    .querySelector(".js-reset-confirm-no")
+    .addEventListener("click", () => {
+      hideResetConfirmation();
+    });
+}
+
+// function for hiding the message
+function hideResetConfirmation() {
+  document.querySelector(".js-reset-confirmation").innerHTML = "";
+}
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
@@ -60,7 +161,7 @@ function playGame(playerMove) {
 
   document.querySelector(".js-result").innerHTML = result;
 
-  document.querySelector(".js-moves").innerHTML = `  You 
+  document.querySelector(".js-moves").innerHTML = `You 
         <img src="${playerMove}-emoji.png" class="move-icon">
         <img src="${computerMove}-emoji.png" class="move-icon">
         Computer`;
